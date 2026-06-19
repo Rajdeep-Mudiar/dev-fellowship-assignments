28294d4058427cb67ad6fdf4f9edb978e7243662
Rajdeep Mudiar <rajdeepmudiar01@gmail.com>
2026-06-19 14:16:35 +0530

    Advanced JavaScript Crash Course completed

---
+++ Task_3_Advanced JavaScript/09_generators/main.js
@@ -0,0 +1,16 @@
+function normalFunction() {
+  console.log("Hello");
+  console.log("World");
+}
+
+normalFunction();
+normalFunction();
+function* generatorFunction() {
+  yield `Hel`;
+  yield `Wor`;
+}
+
+const generatorObject = generatorFunction(); //This generatorObject is an iterator . It can be used in for of loop
+for (const word of generatorObject) {
+  console.log(word);
+}
