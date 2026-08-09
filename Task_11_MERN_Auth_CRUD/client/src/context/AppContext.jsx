bc8d71f6e07d7c291d878d981f88661ea18bdb56
Rajdeep Mudiar <rajdeepmudiar01@gmail.com>
2026-08-09 14:08:11 +0530

    vercel fix

--- Task_11_MERN_Auth_CRUD/client/src/context/AppContext.jsx
+++ Task_11_MERN_Auth_CRUD/client/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContent = createContext();
 export const AppContextProvider = (props) => {
   axios.defaults.withCredentials = true;
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
 
   const [isLoggedin, setIsLoggedin] = useState(false);
   const [userData, setUserData] = useState(false);
